74635af269b4bd0c65add626aa3e96178e858eb2
DanielS <schmieder.d@bbw-rki-berlin.de>
2026-01-14 14:24:25 +0100

    Fix wording.

--- src/pages/StartpagePage.svelte
+++ src/pages/StartpagePage.svelte
@@ -7,7 +7,7 @@
         <a href="https://ffmpegwasm.netlify.app/" target="_blank">ffmpeg.wasm</a>
         <div>
             without any server-side processing.
-            So your files you edit are all saved within your
+            So your files are all saved within your
             browser and will never be uploaded.
         </div>
     </div>
@@ -18,20 +18,24 @@
             pages which all feature different ffmpeg tools.
         </div>
         <div>
-            2. On the top of the page you can select the file you want to process.
-            Note that each sub page you are on filters the input file types on its own which means it can happen that
-            you have files selected but these can't be processed.
+            2. Use the top selector to choose your file.
+            Because each sub-page only supports certain formats, some selected
+            files may not be available for processing.
         </div>
         <div>
             3. After processing files the output will be shown at the bottom of the page.
         </div>
     </div>
-    <div class="card todo">
-        <h2>TODO for me</h2>
-        <div>1. Can't process files twice. A feature where the processed files will be re-applied to the input files would be useful for chaining multiple tools.</div>
-        <div>2. Make error handling easier like a abstract method which checks file properties (e.g. available audio or video stream)</div>
-        <div>3. Create a toast message component which can show important messages.</div>
-    </div>
+<!--    <div class="card todo">-->
+<!--        <h2>TODO for me</h2>-->
+<!--        <div>1. Can't process files twice. A feature where the processed files will be re-applied to the input files-->
+<!--            would be useful for chaining multiple tools.-->
+<!--        </div>-->
+<!--        <div>2. Make error handling easier like a abstract method which checks file properties (e.g. available audio or-->
+<!--            video stream)-->
+<!--        </div>-->
+<!--        <div>3. Create a toast message component which can show important messages.</div>-->
+<!--    </div>-->
 </main>
 
 <style>
